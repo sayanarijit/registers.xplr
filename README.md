d789177b501398dcaff2d7c6a13ae7eb1bb8c578
Arijit Basu <sayanarijit@gmail.com>
2022-05-19 18:37:01 +0530

    Init

--- README.md
+++ README.md
@@ -1,15 +1,29 @@
-# xplr plugin template
+# registers.xplr
 
-Use this template to [write your own xplr plugin](https://arijitbasu.in/xplr/en/writing-plugins.html).
+Use multiple registers to store the selected paths.
 
-> **NOTE:** The `src` directory is a symlink to `.` for compatibility reasons.
-> It may be removed in the future.
+## Usage example
+
+- Select paths usinhg <kbd> space </kbd>.
+- Type <kbd> " </kbd> <kbd> a </kbd> to put the selection into `a` register.
+- Select some more paths.
+- Type <kbd> " </kbd> <kbd> a </kbd> again to swap the selection with `a` register's content.
+- Press <kbd> ctrl-u </kbd> to unselect all.
+- Type <kbd> " </kbd> <kbd> a </kbd> to move the selection from `a` register to the selection list.
 
 ## Requirements
 
-- Some tool
+none
 
 ## Installation
+
+### Install using xpm.xplr
+
+```lua
+require("xpm").setup({
+  { name = 'sayanarijit/registers.xplr' },
+})
+```
 
 ### Install manually
 
@@ -29,24 +43,25 @@ Use this template to [write your own xplr plugin](https://arijitbasu.in/xplr/en/
   ```bash
   mkdir -p ~/.config/xplr/plugins
 
-  git clone https://github.com/{username}/{plugin}.xplr ~/.config/xplr/plugins/{plugin}
+  git clone https://github.com/sayanarijit/registers.xplr ~/.config/xplr/plugins/registers
   ```
 
 - Require the module in `~/.config/xplr/init.lua`
 
   ```lua
-  require("{plugin}").setup()
+  require("registers").setup()
 
   -- Or
 
-  require("{plugin}").setup{
-    mode = "action",
-    key = ":",
+  require("registers").setup{
+    mode = "default",
+    key = '"',
   }
 
-  -- Type `::` and enjoy.
+  -- Type `"` and then another character to swap the selection with a register.
   ```
 
 ## Features
 
-- Some cool feature
+- Swap the current selection with any register.
+- See the active registers with selection count.
--- init.lua
+++ init.lua
@@ -2,12 +2,137 @@
 local xplr = xplr
 ---@diagnostic enable
 
-local util = require("plugin-template1.lib.util")
+local registers = {}
 
-local function setup(args)
-  print(util.dump(args))
-  print(util.dump(xplr))
-  io.read()
+local function format(reg)
+  return "\x1b[1;7m " .. reg .. " \x1b[0m"
 end
 
-return { setup = setup }
+local function get(reg)
+  return registers[reg] or {}
+end
+
+local function set(reg, paths)
+  if paths and #paths ~= 0 then
+    registers[reg] = paths
+  else
+    registers[reg] = nil
+  end
+end
+
+local function all()
+  return registers
+end
+
+local function setup(args)
+  args = args or {}
+  args.mode = args.mode or "default"
+  args.key = args.key or '"'
+
+  xplr.config.modes.builtin[args.mode].key_bindings.on_key[args.key] = {
+    help = "registers",
+    messages = {
+      "PopMode",
+      { SwitchModeCustom = "registers" },
+    },
+  }
+
+  xplr.config.modes.custom.registers = {
+    name = "registers",
+    layout = {
+      Horizontal = {
+        config = {
+          constraints = {
+            { Percentage = 50 },
+            { Percentage = 50 },
+          },
+        },
+        splits = {
+          {
+            CustomContent = {
+              title = "registers",
+              body = {
+                DynamicTable = {
+                  widths = {
+                    { Length = 5 },
+                    { MinLessThanLayoutWidth = 5 },
+                  },
+                  render = "custom.registers.render",
+                },
+              },
+            },
+          },
+          "Selection",
+        },
+      },
+    },
+    key_bindings = {
+      on_key = {
+        esc = {
+          help = "cancel",
+          messages = {
+            "PopMode",
+          },
+        },
+        ["ctrl-c"] = {
+          messages = {
+            "Terminate",
+          },
+        },
+      },
+      on_character = {
+        help = "swap with register",
+        messages = {
+          "UpdateInputBufferFromKey",
+          { CallLuaSilently = "custom.registers.swap" },
+          "PopMode",
+        },
+      },
+    },
+  }
+
+  xplr.fn.custom.registers = {}
+
+  xplr.fn.custom.registers.swap = function(app)
+    local reg = app.input_buffer
+
+    local msgs = {
+      "ClearSelection",
+      "ResetInputBuffer",
+      { LogInfo = string.format("Selection swapped with register %q", reg) },
+    }
+
+    local selection = get(reg)
+
+    for _, path in ipairs(selection) do
+      table.insert(msgs, { SelectPath = path })
+    end
+
+    selection = {}
+
+    for _, node in ipairs(app.selection) do
+      table.insert(selection, node.absolute_path)
+    end
+
+    set(reg, selection)
+
+    return msgs
+  end
+
+  xplr.fn.custom.registers.render = function(_)
+    local view = {
+      { "reg", "# selected" },
+      { "───", "──────────" },
+    }
+
+    local _registers = all()
+    table.sort(_registers)
+    for reg, selection in pairs(_registers) do
+      table.insert(view, { format(reg), tostring(#selection) })
+    end
+
+    return view
+  end
+end
+
+return { setup = setup, get = get, set = set, all = all }
